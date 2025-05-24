410256aa9d37297754fbb60b1fe90963a8c394bf
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2025-05-24 14:23:11 +0530

    Added form handling code

--- src/view/pages/Contact/Contact.tsx
+++ src/view/pages/Contact/Contact.tsx
@@ -1,7 +1,54 @@
+import './Contact.css';
+import {useForm} from "react-hook-form";
+
+type FormData = {
+    email: string;
+    subject: string;
+    message: string;
+}
+
 export function Contact() {
+
+    const {register,
+        handleSubmit,
+        formState: { errors
+    }} = useForm<FormData>();
+
     return (
-        <>
-            <h1>This is Contact Page!</h1>
-        </>
+        <div className="form-container">
+            <h2>Contact Us</h2>
+            <form className="contact-form">
+                <div className="form-group">
+                    <label>Email: </label>
+                    <input type="email"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                message: 'Invalid email format'
+                            }
+                        })
+                        }/>
+                </div>
+                <div className="form-group">
+                    <label>Subject: </label>
+                    <input type="text"
+                        {...register('subject', {
+                            required: 'Subject is required',
+                            pattern: {
+                                value: /^.{10,30}$/,
+                                message: 'Subject must be ' +
+                                    'in between 10 to 30 characters'
+                            }
+                        })}/>
+                </div>
+                <div className="form-group">
+                    <label>Message: </label>
+                    <textarea rows="5"/>
+                </div>
+                <button type="submit"
+                      className="submit-btn">Submit</button>
+            </form>
+        </div>
     );
 }
