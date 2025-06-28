ad50a7ef8b43e48aabc89b357324c093045a4f27
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2025-06-28 13:01:32 +0530

    Added latest changes

--- src/view/common/Product/Product.tsx
+++ src/view/common/Product/Product.tsx
@@ -1,8 +1,7 @@
-import {useState} from "react";
 import {ModifyCart} from "../ModifyCart/ModifyCart.tsx";
 import type {ProductData} from "../../../model/ProductData.ts";
-import {useDispatch} from "react-redux";
-import type {AppDispatch} from "../../../store/store.ts";
+import {useDispatch, useSelector} from "react-redux";
+import type {AppDispatch, RootState} from "../../../store/store.ts";
 import {addItemToCart} from "../../../slices/cartSlice.ts";
 
 type ProductProps = {
@@ -18,11 +17,14 @@ export function Product({data}: ProductProps) {
     const image = images[`../../../assets/products/${data.image}`];
 
     const dispatch = useDispatch<AppDispatch>();
-
-    const [isActive, setIsActive] = useState(false);
+    const item = useSelector(
+        (state: RootState) =>
+               state.cart.items.find(cartItem =>
+                  cartItem.product.id === data.id));
+    // const [isActive, setIsActive] = useState(false);
     const addToCart = () => {
         dispatch(addItemToCart(data));
-        setIsActive(true);
+        // setIsActive(true);
     }
 
     return (
@@ -46,7 +48,7 @@ export function Product({data}: ProductProps) {
             </div>
             <div className="flex justify-center">
                 {
-                    isActive ? (
+                    item ? (
                         <ModifyCart data={data}/>
                     ) : (
                         <button className="w-full mt-4
